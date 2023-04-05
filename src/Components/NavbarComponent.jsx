a09ef5db121a8a9bd4f4499671dd7f7289f9347f
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2023-04-05 15:19:20 +0700

    first commit

--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -11,7 +11,7 @@ const NavbarComponent = () => {
       >
         <Navbar.Brand>
           <img
-            src="/images/logo-UAD.PNG"
+            src="/images/logo-UAD.png"
             className="mr-3 h-12"
             alt="Logo"
           />
